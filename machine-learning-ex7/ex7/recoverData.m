750918b92ea273066f8239c16affcd8c789b57ab
Xiao Nan <xnone0104@gmail.com>
2018-03-02 16:01:13 +0800

    improve code

--- machine-learning-ex7/ex7/recoverData.m
+++ machine-learning-ex7/ex7/recoverData.m
@@ -25,9 +25,7 @@ X_rec = zeros(size(Z, 1), size(U, 1));
 
 for i = 1:size(Z, 1)
     for j = 1:size(U, 1)
-        v = Z(i, :)';
-        recovered_j = v' * U(j, 1:K)';
-        X_rec(i,j) = recovered_j;
+        X_rec(i,j) = Z(i, :) * U(j, 1:K)';
     end
 end
 
