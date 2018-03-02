d8639949354e5407d751b223854685c21477b5b2
Xiao Nan <xnone0104@gmail.com>
2018-03-02 14:52:46 +0800

    simplfy script

--- machine-learning-ex7/ex7/projectData.m
+++ machine-learning-ex7/ex7/projectData.m
@@ -22,9 +22,7 @@ Z = zeros(size(X, 1), K);
 
 for i=1:size(X, 1)
     for j=1:K
-        x = X(i, :)';
-        projection_k = x' * U(:, j);
-        Z(i,j) = projection_k;
+        Z(i,j) = X(i, :) * U(:, j);
     end
 end
 
